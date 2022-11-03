aa37456ff891cc2be4b3ee499677d180db9641c9
matej <matej.basta@email.cz>
2022-11-03 11:13:54 +0100

    initial setup

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -6,6 +7,16 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
+        while(scanner.hasNext()) {
+            String word = scanner.next();
+
+            Pattern pattern = Pattern.compile("[a,e,i,o,u]");
+            String[] syllables = pattern.split(word);
+
+            for (String syllable : syllables) {
+                System.out.println(syllable);
+            }
+        }
 
         scanner.close();
 
